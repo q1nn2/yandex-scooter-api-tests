1677f75da7ee17f47d1290dc28e9e15389982986
q1nn2 <90451521+q1nn2@users.noreply.github.com>
2026-09-20 20:47:08 +0300

    Добавлены адреса API

---
+++ urls.py
@@ -0,0 +1,5 @@
+BASE_URL = 'https://qa-scooter.praktikum-services.ru/api'
+
+CREATE_COURIER = f'{BASE_URL}/v1/courier'
+LOGIN_COURIER = f'{BASE_URL}/v1/courier/login'
+ORDERS = f'{BASE_URL}/v1/orders'
