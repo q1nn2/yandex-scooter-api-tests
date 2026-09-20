8ae22b14e41567c5411b2aca0e1c976fdb957d54
q1nn2 <90451521+q1nn2@users.noreply.github.com>
2026-09-20 22:07:59 +0300

    Уточнены проверки принятия заказа

--- tests/test_accept_order.py
+++ tests/test_accept_order.py
@@ -27,34 +27,34 @@ class TestAcceptOrder:
         response = requests.put(f'{ACCEPT_ORDER}/{order["id"]}')
 
         assert response.status_code == 400
-        assert "message" in response.json()
+        assert response.json()["message"] == "Недостаточно данных для поиска"
 
     @allure.title("С неверным id курьера заказ принять нельзя")
     def test_accept_order_with_wrong_courier_id_error(self, order):
         response = requests.put(
             f'{ACCEPT_ORDER}/{order["id"]}',
-            params={"courierId": 999999999999}
+            params={"courierId": 99999999}
         )
 
         assert response.status_code == 404
-        assert "message" in response.json()
+        assert response.json()["message"] == "Курьера с таким id не существует"
 
     @allure.title("Без id заказа запрос возвращает ошибку")
     def test_accept_order_without_order_id_error(self, courier):
         response = requests.put(
-            f'{ACCEPT_ORDER}/',
+            ACCEPT_ORDER,
             params={"courierId": courier["id"]}
         )
 
         assert response.status_code == 400
-        assert "message" in response.json()
+        assert response.json()["message"] == "Недостаточно данных для поиска"
 
     @allure.title("С неверным id заказа запрос возвращает ошибку")
     def test_accept_order_with_wrong_order_id_error(self, courier):
         response = requests.put(
-            f'{ACCEPT_ORDER}/999999999999',
+            f'{ACCEPT_ORDER}/99999999',
             params={"courierId": courier["id"]}
         )
 
         assert response.status_code == 404
-        assert "message" in response.json()
+        assert response.json()["message"] == "Заказа с таким id не существует"
