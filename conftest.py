1058df78cba9ac3e0bee85fa4e8dee5354daa57a
q1nn2 <90451521+q1nn2@users.noreply.github.com>
2026-09-20 21:34:08 +0300

    Настроена независимая подготовка и очистка тестовых данных

--- conftest.py
+++ conftest.py
@@ -1,25 +1,17 @@
 import pytest
-import requests
 
-from helpers import create_courier, delete_courier, get_courier_id, create_order, get_order_id
-from urls import ORDERS
+from helpers import (
+    create_courier,
+    delete_courier_by_id,
+    get_courier_id,
+    create_order,
+    get_order_id,
+    cancel_order
+)
 
 
 @pytest.fixture
 def courier():
-    login, password, first_name = create_courier()
-
-    yield {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
-
-    delete_courier(login, password)
-
-
-@pytest.fixture
-def courier_with_id():
     login, password, first_name = create_courier()
     courier_id = get_courier_id(login, password)
 
@@ -30,20 +22,16 @@ def courier_with_id():
         "id": courier_id
     }
 
-    delete_courier(login, password)
+    delete_courier_by_id(courier_id)
 
 
 @pytest.fixture
 def order():
     track = create_order()
-    order_data = {
+
+    yield {
         "track": track,
         "id": get_order_id(track)
     }
 
-    yield order_data
-
-    try:
-        requests.put(f'{ORDERS}/cancel', params={"track": track}, timeout=5)
-    except requests.RequestException:
-        pass
+    cancel_order(track)
