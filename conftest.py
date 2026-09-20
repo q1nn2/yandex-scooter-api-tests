5c5dc94684d3c15c53db6f1f6865cf0c4d53a44c
q1nn2 <90451521+q1nn2@users.noreply.github.com>
2026-09-20 20:57:36 +0300

    Добавлены фикстуры для дополнительных проверок

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from helpers import create_courier, delete_courier
+from helpers import create_courier, delete_courier, get_courier_id, create_order, get_order_id
 
 
 @pytest.fixture
@@ -14,3 +14,27 @@ def courier():
     }
 
     delete_courier(login, password)
+
+
+@pytest.fixture
+def courier_with_id():
+    login, password, first_name = create_courier()
+    courier_id = get_courier_id(login, password)
+
+    yield {
+        "login": login,
+        "password": password,
+        "firstName": first_name,
+        "id": courier_id
+    }
+
+    delete_courier(login, password)
+
+
+@pytest.fixture
+def order():
+    track = create_order()
+    return {
+        "track": track,
+        "id": get_order_id(track)
+    }
