aaf77f3cd3c495bdaa439be360a3af112119b762
q1nn2 <90451521+q1nn2@users.noreply.github.com>
2026-09-20 20:57:54 +0300

    Добавлены тесты получения заказа по номеру

---
+++ tests/test_get_order_by_track.py
@@ -0,0 +1,30 @@
+import allure
+import requests
+
+from urls import TRACK_ORDER
+
+
+@allure.feature("Получение заказа по номеру")
+class TestGetOrderByTrack:
+
+    @allure.title("Заказ можно получить по его номеру")
+    def test_get_order_by_track_success(self, order):
+        response = requests.get(TRACK_ORDER, params={"t": order["track"]})
+
+        assert response.status_code == 200
+        assert "order" in response.json()
+        assert response.json()["order"]["track"] == order["track"]
+
+    @allure.title("Без номера заказа запрос возвращает ошибку")
+    def test_get_order_without_track_error(self):
+        response = requests.get(TRACK_ORDER)
+
+        assert response.status_code == 400
+        assert "message" in response.json()
+
+    @allure.title("Несуществующий номер заказа возвращает ошибку")
+    def test_get_order_with_nonexistent_track_error(self):
+        response = requests.get(TRACK_ORDER, params={"t": 999999999999})
+
+        assert response.status_code == 404
+        assert "message" in response.json()
