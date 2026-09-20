421128045db604e23909bf15ac245e5ccc44ed5a
q1nn2 <90451521+q1nn2@users.noreply.github.com>
2026-09-20 20:47:19 +0300

    Добавлены тесты создания курьера

---
+++ tests/test_create_courier.py
@@ -0,0 +1,46 @@
+import allure
+import requests
+
+from helpers import generate_random_string, get_courier_id
+from urls import CREATE_COURIER
+
+
+@allure.feature("Создание курьера")
+class TestCreateCourier:
+
+    @allure.title("Курьера можно создать")
+    def test_create_courier_success(self):
+        payload = {
+            "login": generate_random_string(10),
+            "password": generate_random_string(10),
+            "firstName": generate_random_string(10)
+        }
+
+        response = requests.post(CREATE_COURIER, data=payload)
+
+        try:
+            assert response.status_code == 201
+            assert response.json() == {"ok": True}
+        finally:
+            if response.status_code == 201:
+                courier_id = get_courier_id(payload["login"], payload["password"])
+                requests.delete(f'{CREATE_COURIER}/{courier_id}')
+
+    @allure.title("Нельзя создать двух одинаковых курьеров")
+    def test_create_duplicate_courier_error(self, courier):
+        response = requests.post(CREATE_COURIER, data=courier)
+
+        assert response.status_code == 409
+        assert response.json()["message"] == "Этот логин уже используется"
+
+    @allure.title("Без обязательного поля курьер не создаётся")
+    def test_create_courier_without_required_field_error(self):
+        payload = {
+            "password": generate_random_string(10),
+            "firstName": generate_random_string(10)
+        }
+
+        response = requests.post(CREATE_COURIER, data=payload)
+
+        assert response.status_code == 400
+        assert response.json()["message"] == "Недостаточно данных для создания учетной записи"
